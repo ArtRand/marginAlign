6ca18e9d062b69fa8408f4eb89787b9e05d08f28
ArtRand <arand@soe.ucsc.edu>
2016-12-21 09:55:36 -0800

    refactored cPecan Docker input, seems to work ok

--- Docker/cPecan/runner.py
+++ Docker/cPecan/runner.py
@@ -2,11 +2,12 @@
 """Wrapper script for cPecanRealign in Docker
 """
 import os
+import sys
 import uuid
 import cPickle
 from argparse import ArgumentParser
 
-print("running.. RUNNER!! with pickle")
+print("running.. RUNNER!!")
 
 
 def fasta_write(file_path, seq, seq_label):
@@ -21,10 +22,11 @@ def fasta_write(file_path, seq, seq_label):
 def main():
     parser = ArgumentParser()
     # input files
-    parser.add_argument("--exonerate_file", action="store", dest="exonerate_file", required=True)
-    parser.add_argument("--reference", action="store", dest="reference_fasta", required=True)
-    parser.add_argument("--query_seqs", action="store", dest="query_seqs", required=True)
-    parser.add_argument("--query_labels", action="store", dest="query_labels", required=True)
+    parser.add_argument("--input", action="store", dest="input_params", required=True)
+    #parser.add_argument("--exonerate_file", action="store", dest="exonerate_file", required=True)
+    #parser.add_argument("--reference", action="store", dest="reference_fasta", required=True)
+    #parser.add_argument("--query_seqs", action="store", dest="query_seqs", required=True)
+    #parser.add_argument("--query_labels", action="store", dest="query_labels", required=True)
     parser.add_argument("--hmm_file", action="store", dest="hmm_file", required=True)
     # input parameters
     parser.add_argument("--gap_gamma", action="store", type=float, required=True)
@@ -32,28 +34,40 @@ def main():
     # output and environment
     parser.add_argument("--output_alignment_file", action="store", dest="out", required=True)
     parser.add_argument("--work_dir", action="store", dest="work_dir", default="/data/", required=False)
+    parser.add_argument("--test_container", action="store_true", dest="test_container", default=False)
     args = parser.parse_args()
 
-    assert(os.path.exists(args.exonerate_file)), "[cPecan::runner]ERROR didn't find exonerate file "\
-                                                 "looked {}".format(args.exonerate_file)
-    assert(os.path.exists(args.reference_fasta)), "[cPecan::runner]ERROR didn't find reference FASTA "\
-                                                  "looked {}".format(args.reference_fasta)
-    assert(os.path.exists(args.query_file)), "[cPecan::runner]ERROR didn't find query reads file "\
-                                             "looked {}".format(args.query_file)
+    if args.test_container:
+        os.system("cPecanRealign --help")
+        sys.exit(0)
+    with open(args.input_params, 'r') as fH:
+        params = cPickle.load(fH)
+#    assert(os.path.exists(args.exonerate_file)), "[cPecan::runner]ERROR didn't find exonerate file "\
+#                                                 "looked {}".format(args.exonerate_file)
+#    assert(os.path.exists(args.reference_fasta)), "[cPecan::runner]ERROR didn't find reference FASTA "\
+#                                                  "looked {}".format(args.reference_fasta)
+#    assert(os.path.exists(args.query_file)), "[cPecan::runner]ERROR didn't find query reads file "\
+#                                             "looked {}".format(args.query_file)
 
     # intermediate files
-    uid = uuid.uuidt().hex
-    temp_read        = args.work_dir + "read{}.fa".format(uid)
-    exonerate_cigars = open(args.exonerate_file, 'r')
-    read_sequences   = open(args.query_seqs, 'r')
-    read_labels      = open(args.query_labels, 'r')
+    uid = uuid.uuid4().hex
+    temp_read_fn      = args.work_dir + "read.{}.fa".format(uid)
+    temp_reference_fn = args.work_dir + "reference.{}.fa".format(uid)
+    exonerate_cigars  = params["exonerate_cigars"]
+    read_sequences    = params["query_sequences"]
+    read_labels       = params["query_labels"]
 
+    # write the reference for cPecan 
+    fasta_write(file_path=temp_reference_fn, seq=params["contig_seq"], seq_label=params["contig_name"])
+
+    # this loop aligns each read, to the reference with the exonerate pairwise alignment as the
+    # guide alignment
     for cig, seq, lab in zip(exonerate_cigars, read_sequences, read_labels):
-        fasta_write(file_path=temp_read, seq=seq, seq_label=lab)
-        cmd = "echo \"{cig}\" | cPecanRealign {ref} {read} --diagonalExpansion=10 "\
+        fasta_write(file_path=temp_read_fn, seq=seq, seq_label=lab)
+        cmd = "echo \"{cig}\" | cPecanRealign {ref} {query} --diagonalExpansion=10 "\
               "--splitMatrixBiggerThanThis=3000 --loadHmm {hmm} --gapGamma={gapG} "\
               "--matchGamma={matchG} >> {out}"
-        os.system(cmd.format(cig=cig, ref=args.reference_fasta, read=temp_read, hmm=args.hmm_file,
+        os.system(cmd.format(cig=cig, ref=temp_reference_fn, query=temp_read_fn, hmm=args.hmm_file,
                              gapG=args.gap_gamma, matchG=args.match_gamma, out=args.out))
 
 if __name__ == "__main__":
--- src/margin/toil/realign.py
+++ src/margin/toil/realign.py
@@ -4,47 +4,32 @@ import sys
 import os
 import pysam
 import uuid
+import cPickle
 import toil_lib.programs as tlp
-from margin.toil.localFileManager import LocalFileManager
-from margin.utils import samIterator, getExonerateCigarFormatString, getFastaDictionary, fastaWrite
+from margin.utils import samIterator, getExonerateCigarFormatString, getFastaDictionary
 
 
 DEBUG = True
 DOCKER_DIR = "/data/"
 
 
-class TempBatchFileHandler(object):
+class LocalFile(object):
     """A struct containing the path and handle for a file, used to easily access the a file and
     it's contents
     """
-    def __init__(self, path):
-        self.path     = path
-        self.is_open  = False
-        self._handle  = None
+    def __init__(self, workdir, filename):
+        self.path     = workdir + "/" + filename
+        self.filename = filename
+        self.workdir  = workdir
 
-    def handle_getter(self):
-        if not self.is_open:
-            self.__open()
-        return self._handle
+    def filenameGetter(self):
+        return self.filename
 
-    def __open(self):
-        self._handle  = open(self.path, 'wa')
-        self.is_open = True
-
-    def path_getter(self):
+    def fullpathGetter(self):
         return self.path
 
-    def close(self):
-        if self.is_open:
-            self._handle.close()
-
-    def exists(self):
-        return os.path.exists(self.path)
-
-    def write(self, str):
-        if not self.is_open:
-            self.__open()
-        self._handle.write(str)
+    def workdirGetter(self):
+        return self.workdir
 
 
 def cPecanRealignJobFunction(job, global_config, job_config,
@@ -53,37 +38,33 @@ def cPecanRealignJobFunction(job, global_config, job_config,
     """Runs Docker-ized cPecan Hmm
     """
     uid = uuid.uuid4().hex
-    # make fasta with the config
-    contig_fasta_fn = "ref.{}.fa".format(uid)
-    contig_fasta    = job.fileStore.getLocalTempDir() + "/" + contig_fasta_fn
 
-    with open(contig_fasta, 'w') as fH:
-        fastaWrite(fH, job_config["contig_name"], job_config["contig_seq"])
+    # need a local directory to pass to docker
+    workdir = job.fileStore.getLocalTempDir()
+    # need the hmm file for cPecan 
+    local_hmm    = LocalFile(workdir=workdir, filename="hmm.{}.txt".format(uid))
+    local_output = LocalFile(workdir=workdir, filename="cPecan_out.{}.txt".format(uid))
+    # copy the hmm file from the FileStore to local workdir
+    job.fileStore.readGlobalFile(fileStoreID=global_config["hmm_file"], userPath=local_hmm.fullpathGetter())
+    # need a local file to put the output of the alignments
 
-    # get the files locally, we need:
-    out_fid = job.fileStore.getLocalTempFile()
-    fids_to_get = {  # map of FileStoreIDs to filenames
-        job_config["exonerate_fid"]  : "exonerate_file.{}.tmp".format(uid),
-        job_config["query_seqs_fid"] : "query_seqs.{}.tmp".format(uid),
-        job_config["query_labs_fid"] : "query_labs.{}.tmp".format(uid),
-        global_config["hmm_file"]    : "in_hmm.{}.tmp".format(uid),
-    }
-    localFiles = LocalFileManager(job, fids_to_get.keys(), fids_to_get)
+    if DEBUG:
+        job.fileStore.logToMaster("local hmm %s local output %s" % (local_hmm.fullpathGetter(), local_output.fullpathGetter()))
 
-    # run cPecan in a container
-    exonerate_arg   = "--exonerate_file {}".format(DOCKER_DIR + localFiles.localFileName(job_config["exonerate_fid"]))
-    reference_arg   = "--reference {} ".format(DOCKER_DIR + contig_fasta_fn)
-    query_seqs_arg  = "--query_seqs {} ".format(DOCKER_DIR + localFiles.localFileName(job_config["query_seqs_fid"]))
-    query_labs_arg  = "--query_labels {} ".format(DOCKER_DIR + localFiles.localFileName(job_config["query_labs_fid"]))
-    hmm_arg         = "--hmm_file {} ".format(DOCKER_DIR + localFiles.localFileName(global_config["hmm_file"]))
-    gap_gamma_arg   = "--gap_gamma {} ".format(global_config["gap_gamma"])
-    match_gamma_arg = "--match_gamma {} ".format(global_config["match_gamma"])
-    ## TODO left off here, need a good way to aggregate `tempOutputFiles`
-    output_arg      = "--output_alignment_file {} ".format()
+    # pickle the job_config, that contains the reference sequence, the query sequences, and the pairwise alignments
+    local_input_obj = LocalFile(workdir=workdir, filename="cPecanInput.{}.pkl".format(uid))
+    with open(local_input_obj.fullpathGetter(), "w") as fH:
+        cPickle.dump(job_config, fH)
 
-    #cPecan_parameters = []
-    #tlp.docker_call(tool=cPecan_image, parameters=
+    input_arg       = "--input={}".format(DOCKER_DIR + local_input_obj.filenameGetter())
+    hmm_arg         = "--hmm_file={}".format(DOCKER_DIR + local_hmm.filenameGetter())
+    gap_gamma_arg   = "--gap_gamma={}".format(global_config["gap_gamma"])
+    match_gamma_arg = "--match_gamma={}".format(global_config["match_gamma"])
+    output_arg      = "--output_alignment_file={}".format(DOCKER_DIR + local_output.filenameGetter())
 
+    cPecan_parameters = [input_arg, hmm_arg, gap_gamma_arg, match_gamma_arg, output_arg]
+    tlp.docker_call(tool=cPecan_image, parameters=cPecan_parameters, work_dir=(workdir + "/"))
+    job.fileStore.logToMaster("DUMMY")
 
 
 def realignSamFile(job, config, chained_alignment_output):
@@ -103,26 +84,21 @@ def realignSamFile(job, config, chained_alignment_output):
 
     def send_alignment_batch():
         if exonerate_cigar_batch is not None:
-            # import the files to the FileStore
             if DEBUG:
                 job.fileStore.logToMaster("[realignSamFile::send_alignment_batch] adding child job")
 
-            assert(exonerate_cigar_batch.exists()), "[realignSamFile]ERROR didn't find exonerate file"
-            assert(query_seqs.exists()), "[realignSamFile]ERROR didn't find query seqs file"
-            assert(query_labs.exists()), "[realignSamFile]ERROR didn't find query labels file"
-            exonerate_cigar_batch.close()
-            query_seqs.close()
-            query_labs.close()
-            exonerate_fid  = job.fileStore.writeGlobalFile(exonerate_cigar_batch.path_getter())
-            query_seqs_fid = job.fileStore.writeGlobalFile(query_seqs.path_getter())
-            query_labs_fid = job.fileStore.writeGlobalFile(query_labs.path_getter())
-            job.fileStore.logToMaster("GOING TO ALIGN --> %s %s %s" % (exonerate_fid, query_seqs_fid, query_labs_fid))
+            # import the files to the FileStore
+            assert(len(exonerate_cigar_batch) == len(query_seqs)), "[send_alignment_batch]"\
+                " len(exonerate_cigar_batch) != len(query_seqs)"
+            assert(len(query_seqs) == len(query_labs)), "[send_alignment_batch]"\
+                " len(query_seqs) != len(query_labs)"
+            job.fileStore.logToMaster("GOING TO ALIGN --> %s " % contig_name)
             cPecan_config = {
-                "exonerate_fid"  : exonerate_fid,
-                "query_seqs_fid" : query_seqs_fid,
-                "query_labs_fid" : query_labs_fid,
-                "contig_seq"     : reference_map[contig_name],  # this might be very inefficient for large genomes..?
-                "contig_name"    : contig_name,
+                "exonerate_cigars" : exonerate_cigar_batch,
+                "query_sequences"  : query_seqs,
+                "query_labels"     : query_labs,
+                "contig_seq"       : reference_map[contig_name],  # this might be very inefficient for large genomes..?
+                "contig_name"      : contig_name,
             }
             job.addChildJobFn(cPecanRealignJobFunction, config, cPecan_config)
         else:  # mostly for initial conditions, do nothing
@@ -140,18 +116,19 @@ def realignSamFile(job, config, chained_alignment_output):
            contig_name != sam.getrname(aligned_segment.reference_id)):
             # make a new batch of reads and cigars
             send_alignment_batch()  # send the previous batch to become a child job
-            exonerate_cigar_batch = TempBatchFileHandler(job.fileStore.getLocalTempFile())
-            query_seqs            = TempBatchFileHandler(job.fileStore.getLocalTempFile())
-            query_labs            = TempBatchFileHandler(job.fileStore.getLocalTempFile())
+            # start new batches
+            exonerate_cigar_batch = []
+            query_seqs            = []
+            query_labs            = []
             total_seq_len         = 0
 
         assert(exonerate_cigar_batch is not None), "[realignSamFile]ERROR exonerate_cigar_batch is NONE"
         assert(query_seqs is not None), "[realignSamFile]ERROR query_batch is NONE"
         assert(query_labs is not None), "[realignSamFile]ERROR query_labs is NONE"
 
-        exonerate_cigar_batch.write(getExonerateCigarFormatString(aligned_segment, sam) + "\n")
-        query_seqs.write(aligned_segment.query_sequence + "\n")
-        query_labs.write(aligned_segment.query_name + "\n")
+        exonerate_cigar_batch.append(getExonerateCigarFormatString(aligned_segment, sam) + "\n")
+        query_seqs.append(aligned_segment.query_sequence + "\n")
+        query_labs.append(aligned_segment.query_name + "\n")
         # updates
         total_seq_len += len(aligned_segment.query_sequence)
         contig_name = sam.getrname(aligned_segment.reference_id)

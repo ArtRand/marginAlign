ac47541478f94a210fb6faf6b0bee46a83339262
ArtRand <arand@soe.ucsc.edu>
2017-01-13 18:25:58 -0800

    parallel variant calling, clean

--- src/margin/toil/variantCaller.py
+++ src/margin/toil/variantCaller.py
@@ -3,7 +3,6 @@ import os
 
 from itertools import islice, chain
 
-#from toil_lib import flatten
 from toil_lib import require
 from toil_lib.programs import docker_call
 
@@ -67,27 +66,22 @@ def callVariantsOnBatch(job, config, expectations_batch):
     return variant_calls
 
 
-def writeAndDeliverVCF(job, config, variantCalls, output_label):
-    contig_seqs   = getFastaDictionary(job.fileStore.readGlobalFile(config["reference_FileStoreID"]))
-    workdir       = job.fileStore.getLocalTempDir()
-    output_vcf    = LocalFile(workdir=workdir,
-                              filename="{sample}_{out_label}.vcf".format(sample=config["sample_label"],
-                                                                         out_label=output_label))
+def writeAndDeliverVCF(job, config, nested_variant_calls, output_label):
+    contig_seqs = getFastaDictionary(job.fileStore.readGlobalFile(config["reference_FileStoreID"]))
+    workdir     = job.fileStore.getLocalTempDir()
+    output_vcf  = LocalFile(workdir=workdir,
+                            filename="{sample}_{out_label}.vcf".format(sample=config["sample_label"],
+                                                                       out_label=output_label))
 
-    job.fileStore.logToMaster("[writeAndDeliverVCF]Got %s chunks" % len(variantCalls))
-    job.fileStore.logToMaster("[writeAndDeliverVCF]One looks like %s" % variantCalls[0])
-    variant_calls2 = chain.from_iterable(variantCalls)
+    variant_calls = chain.from_iterable(nested_variant_calls)
 
-    #job.fileStore.logToMaster("[writeAndDeliverVCF]Got %s variant calls" % len(variant_calls2))
-    #job.fileStore.logToMaster("[writeAndDeliverVCF]One looks like %s" % variant_calls2[0])
-
-    vcfWrite(config["ref"], contig_seqs, variant_calls2, output_vcf.fullpathGetter())
+    vcfWrite(config["ref"], contig_seqs, variant_calls, output_vcf.fullpathGetter())
     require(os.path.exists(output_vcf.fullpathGetter()), "[callVariantsWithAlignedPairsJobFunction]Did not make temp VCF file")
 
     if config["debug"]:
-        variant_calls3 = chain.from_iterable(variantCalls)
+        variant_calls2 = chain.from_iterable(nested_variant_calls)
         vcf_calls = vcfRead(output_vcf.fullpathGetter())
-        calls     = set(map(lambda x : (x[0], x[1] + 1, x[2]), list(variant_calls3)))
+        calls     = set(map(lambda x : (x[0], x[1] + 1, x[2]), list(variant_calls2)))
         require(vcf_calls == calls, "[callVariantsWithAlignedPairsJobFunction]vcf write error")
 
     deliverOutput(job, output_vcf, config["output_dir"])
@@ -96,8 +90,8 @@ def writeAndDeliverVCF(job, config, variantCalls, output_label):
 def callVariantsWithAlignedPairsJobFunction(job, config, input_samfile_fid, output_label, cPecan_alignedPairs_fids):
     def chunk_expectations():
         it = iter(expectations_at_each_position)
-        for i in xrange(0, len(expectations_at_each_position), vcf_workers):
-            yield {k: expectations_at_each_position[k] for k in islice(it, vcf_workers)}
+        for i in xrange(0, len(expectations_at_each_position), config["max_variant_call_positions_per_job"]):
+            yield {k: expectations_at_each_position[k] for k in islice(it, config["max_variant_call_positions_per_job"])}
 
     BASES = "ACGT"
     sorted_alignedPair_fids = sortResultsByBatch(cPecan_alignedPairs_fids)
@@ -118,45 +112,14 @@ def callVariantsWithAlignedPairsJobFunction(job, config, input_samfile_fid, outp
                 for b in BASES:
                     expectations_at_each_position[k][b] += posteriors[k][b]
 
-    vcf_workers   = int(len(expectations_at_each_position) / 4)    
     variant_calls = []
-    i = 0
-    for chunk in chunk_expectations():
-        job.fileStore.logToMaster("-->adding chunk %s" % i)
-        variant_calls.append(job.addChildJobFn(callVariantsOnBatch, config, chunk).rv())
-        i += 1
-    #variant_calls = []
-    #contig_seqs   = getFastaDictionary(job.fileStore.readGlobalFile(config["reference_FileStoreID"]))
-    #error_model   = loadHmmSubstitutionMatrix(job.fileStore.readGlobalFile(config["error_model_FileStoreID"]))
-    #evo_sub_mat   = getNullSubstitutionMatrix()
-    #workdir       = job.fileStore.getLocalTempDir()
-    #output_vcf    = LocalFile(workdir=workdir,
-    #                          filename="{sample}_{out_label}.vcf".format(sample=config["sample_label"],
-    #                                                                     out_label=output_label))
+    batch_number  = 0
 
-    #chunks        = [expectations_at_each_position.iteritems()] * vcf_workers
-    #generator     = (dict(ifilter(None, v)) for v in izip_longest(*chunks))
-    #variant_calls = [job.addChildJobFn(callVariantsOnBatch, config, batch).rv() for batch in generator]
+    for chunk in chunk_expectations():
+        variant_calls.append(job.addChildJobFn(callVariantsOnBatch, config, chunk).rv())
+        batch_number += 1
+
+    job.fileStore.logToMaster("[callVariantsWithAlignedPairsJobFunction]Made {} batches for variant calling"
+                              "".format(batch_number))
 
     job.addFollowOnJobFn(writeAndDeliverVCF, config, variant_calls, output_label)
-
-    #for contig, position in expectations_at_each_position:
-    #    ref_base     = contig_seqs[contig][position]
-    #    expectations = expectations_at_each_position[(contig, position)]
-    #    total_prob   = sum(expectations.values())
-    #    require(total_prob > 0, "[callVariantsWithAlignedPairsJobFunction]Total prob == 0")
-    #    posterior_probs = calcBasePosteriorProbs(dict(zip(BASES, map(lambda x : float(expectations[x]) / total_prob, BASES))),
-    #                                             ref_base, evo_sub_mat, error_model)
-    #    for b in BASES:
-    #        if b != ref_base and posterior_probs[b] >= config["variant_threshold"]:
-    #            variant_calls.append((contig, position, b, posterior_probs[b]))
-
-    #vcfWrite(config["ref"], contig_seqs, variant_calls, output_vcf.fullpathGetter())
-    #require(os.path.exists(output_vcf.fullpathGetter()), "[callVariantsWithAlignedPairsJobFunction]Did not make temp VCF file")
-
-    #if config["debug"]:
-    #    vcf_calls = vcfRead(output_vcf.fullpathGetter())
-    #    calls     = set(map(lambda x : (x[0], x[1] + 1, x[2]), variant_calls))
-    #    require(vcf_calls == calls, "[callVariantsWithAlignedPairsJobFunction]vcf write error")
-
-    #deliverOutput(job, output_vcf, config["output_dir"])
